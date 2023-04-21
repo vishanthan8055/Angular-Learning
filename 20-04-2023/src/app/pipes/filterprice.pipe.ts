30cbc4de68395f5bdb2f57ef5d944427fd92547e
vishanthan8055 <vishanthan8055@gmail.com>
2023-04-21 22:06:05 +0530

    add-color

--- 20-04-2023/src/app/pipes/filterprice.pipe.ts
+++ 20-04-2023/src/app/pipes/filterprice.pipe.ts
@@ -8,7 +8,7 @@ export class FilterpricePipe implements PipeTransform {
 
   transform(products: any, min_v: number, max_v:number): any {
     if(max_v===5000){
-      return products.filter((x:any)=>(parseInt(x.price))>=5000)
+      return products.filter((x:any)=>(parseInt(x.price)>=min_v))
     }
     return products.filter((x:any)=>(parseInt(x.price)<=max_v && parseInt(x.price)>=min_v))
   }
